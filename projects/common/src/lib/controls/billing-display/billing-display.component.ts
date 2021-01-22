07e3fd9f36d2b3f856f10f4d1b1962052e3f0fc5
Jack Penhale <jack.penhale@fathym.com>
2021-01-22 13:36:23 -0700

    completed refactor to billing display

--- projects/common/src/lib/controls/billing-display/billing-display.component.ts
+++ projects/common/src/lib/controls/billing-display/billing-display.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { emit } from 'process';
+import { BillingPlanOption } from '../../state/user-billing/user-billing.state';
 
 @Component({
   selector: 'lcu-billing-display',
@@ -9,15 +11,39 @@ export class BillingDisplayComponent implements OnInit {
   //  Fields
 
   //  Properties
+  @Output('buy-now')
+  public BuyNow: EventEmitter<BillingPlanOption>;
+
+  @Input('displayed-plans')
+  public DisplayedPlans: BillingPlanOption[];
+
+  @Input('featured')
+  public FeaturedPlanGroup: string;
+
+  @Input('popular')
+  public PopularPlanGroup: string;
+
+  @Input('show-button')
+  public ShowButton: boolean;
+
+  @Input('plan-options')
+  public PlanOptions: BillingPlanOption[];
 
   //  Constructors
-  constructor() { }
+  constructor() { 
+    this.BuyNow = new EventEmitter();
 
-  //  Life Cycle
-  ngOnInit(): void {
+    this.ShowButton = true;
   }
 
-  // API Methods
+  //  Life Cycle
+  public ngOnInit(): void {
+  }
+
+  //  API Methods
+  public BuyNowClicked(plan: BillingPlanOption) {
+    this.BuyNow.emit(plan);
+  }
 
   //  Helpers
 }
