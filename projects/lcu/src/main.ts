7fd9ed0268d68d1ad9ffae8c87101fbc01eb4617
mcgear <michael.gearhardt@gmail.com>
2021-01-27 16:51:46 -0700

    Hopefully fixed lcu build

--- projects/lcu/src/main.ts
+++ projects/lcu/src/main.ts
@@ -4,7 +4,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-import 'zone.js';
+import 'zone.js/dist/zone';
 
 if (environment.production) {
   enableProdMode();
