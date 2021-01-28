593d0e73f6edb070c0d35dc46917971ccdf035a6
Jack Penhale <jack.penhale@fathym.com>
2021-01-28 15:31:10 -0700

    billing plan option helper & home demo

--- projects/common/src/lib/elements/plan-view/plan-view.component.ts
+++ projects/common/src/lib/elements/plan-view/plan-view.component.ts
@@ -9,7 +9,7 @@ import {
   EventEmitter,
   Output,
 } from '@angular/core';
-import { LCUElementContext, LcuElementComponent } from '@lcu/common';
+import { LCUElementContext, LcuElementComponent, BaseModeledResponse } from '@lcu/common';
 import { BillingPlanOption } from '../../state/user-billing/user-billing.state';
 
 export class LcuBillingPlanViewElementState {}
@@ -31,8 +31,14 @@ export class LcuBillingPlanViewElementComponent
   protected http: HttpClient;
 
   //  Properties
-  @Input('billing-plan-options')
+
   public BillingPlanOptions: (BillingPlanOption & any)[];
+
+  @Input('billing-plan-options-prefix')
+  public BillingPlanOptionsPrefix: (BillingPlanOption & any)[];
+
+  @Input('billing-plan-options-suffix')
+  public BillingPlanOptionsSuffix: (BillingPlanOption & any)[];
 
   @Output('buy-now-click')
   public BuyNowClick: EventEmitter<BillingPlanOption>;
@@ -73,92 +79,13 @@ export class LcuBillingPlanViewElementComponent
 
   //  Helpers
   protected loadBillingOptions() {
-    //this.http.get("https://www.iot-ensemble.com/api/state/usermanagement/ListBillingOptions")
-    this.BillingPlanOptions = [
-      {
-        Description: '',
-        DiscountedFrom: '30',
-        Interval: 'month',
-        Lookup: 'price_1HhIDIEoSnTpuGWGokN2x6wq',
-        Name: 'IoT - Starter ',
-        PlanGroup: 'starter',
-        Price: 20,
-        Priority: '20',
-        LicenseType: 'iot',
-        Popular: 'true',
-        SuccessRedirect: 'https://www.iot-ensemble.com/dashboard',
-        Devices: '10',
-        DataInterval: '60',
-        DataRetention: '259200',
-        HeaderName: 'Fathym | IoT Ensemble',
-        LicenseName: 'Fathym IoT',
-        PlanFeatures:
-          'IoT Management|Shared Cloud|10 Devices|60 second Data Velocity|3 Days of Data Retention',
-        TrialPeriodDays: 0,
-      },
-      {
-        Description: '',
-        DiscountedFrom: '300',
-        Interval: 'year',
-        Lookup: 'price_1HhIDIEoSnTpuGWGTUDzu18s',
-        Name: 'IoT - Starter ',
-        PlanGroup: 'starter',
-        Price: 200,
-        Priority: '20',
-        LicenseType: 'iot',
-        Popular: 'true',
-        SuccessRedirect: 'https://www.iot-ensemble.com/dashboard',
-        Devices: '10',
-        DataInterval: '60',
-        DataRetention: '259200',
-        HeaderName: 'Fathym | IoT Ensemble',
-        LicenseName: 'Fathym IoT',
-        PlanFeatures:
-          'IoT Management|Shared Cloud|10 Devices|60 second Data Velocity|3 Days of Data Retention',
-        TrialPeriodDays: 0,
-      },
-      {
-        Description: '',
-        DiscountedFrom: '100',
-        Interval: 'month',
-        Lookup: 'price_1HhIGXEoSnTpuGWGwN29a2zo',
-        Name: 'IoT - Pro ',
-        PlanGroup: 'pro',
-        Price: 70,
-        Priority: '30',
-        LicenseType: 'iot',
-        Featured: 'true',
-        SuccessRedirect: 'https://www.iot-ensemble.com/dashboard',
-        Devices: '50',
-        DataInterval: '30',
-        DataRetention: '604800',
-        HeaderName: 'Fathym | IoT Ensemble',
-        LicenseName: 'Fathym IoT',
-        PlanFeatures:
-          'IoT Management|Shared Cloud|50 Devices|30 second Data Velocity|7 Days of Data Retention',
-        TrialPeriodDays: 0,
-      },
-      {
-        Description: '',
-        DiscountedFrom: '1000',
-        Interval: 'year',
-        Lookup: 'price_1HhIGXEoSnTpuGWGMLhRZtbK',
-        Name: 'IoT - Pro ',
-        PlanGroup: 'pro',
-        Price: 700,
-        Priority: '30',
-        LicenseType: 'iot',
-        Featured: 'true',
-        SuccessRedirect: 'https://www.iot-ensemble.com/dashboard',
-        Devices: '50',
-        DataInterval: '30',
-        DataRetention: '604800',
-        HeaderName: 'Fathym | IoT Ensemble',
-        LicenseName: 'Fathym IoT',
-        PlanFeatures:
-          'IoT Management|Shared Cloud|50 Devices|30 second Data Velocity|7 Days of Data Retention',
-        TrialPeriodDays: 0,
-      },
-    ];
+    // tslint:disable-next-line:max-line-length
+    this.http.get('https://www.iot-ensemble.com/api/state/usermanagement/ListBillingOptions?licenseType=${this.LiscenseType}').subscribe((result: BaseModeledResponse<BillingPlanOption[]>)=>{
+      this.BillingPlanOptions = [
+        ...(this.BillingPlanOptionsPrefix || []),
+        ...(result.Model || []),
+        ...(this.BillingPlanOptionsSuffix || [])
+      ]
+    });
   }
 }
--- projects/demo/src/app/controls/home/home.component.ts
+++ projects/demo/src/app/controls/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { BillingPlanOption } from 'projects/common/src/lib/state/user-billing/user-billing.state';
+
 
 @Component({
   selector: 'lcu-home',
@@ -7,7 +9,96 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  public BillingPlanArray: (BillingPlanOption & any)[];
+
+  constructor() {
+    this.BillingPlanArray = [
+      {
+        Description: '',
+        DiscountedFrom: '30',
+        Interval: 'month',
+        Lookup: 'price_1HhIDIEoSnTpuGWGokN2x6wq',
+        Name: 'IoT - Starter ',
+        PlanGroup: 'starter',
+        Price: 20,
+        Priority: '20',
+        LicenseType: 'iot',
+        Popular: 'true',
+        SuccessRedirect: 'https://www.iot-ensemble.com/dashboard',
+        Devices: '10',
+        DataInterval: '60',
+        DataRetention: '259200',
+        HeaderName: 'Fathym | IoT Ensemble',
+        LicenseName: 'Fathym IoT',
+        PlanFeatures:
+          'IoT Management|Shared Cloud|10 Devices|60 second Data Velocity|3 Days of Data Retention',
+        TrialPeriodDays: 0,
+      },
+      {
+        Description: '',
+        DiscountedFrom: '300',
+        Interval: 'year',
+        Lookup: 'price_1HhIDIEoSnTpuGWGTUDzu18s',
+        Name: 'IoT - Starter ',
+        PlanGroup: 'starter',
+        Price: 200,
+        Priority: '20',
+        LicenseType: 'iot',
+        Popular: 'true',
+        SuccessRedirect: 'https://www.iot-ensemble.com/dashboard',
+        Devices: '10',
+        DataInterval: '60',
+        DataRetention: '259200',
+        HeaderName: 'Fathym | IoT Ensemble',
+        LicenseName: 'Fathym IoT',
+        PlanFeatures:
+          'IoT Management|Shared Cloud|10 Devices|60 second Data Velocity|3 Days of Data Retention',
+        TrialPeriodDays: 0,
+      },
+      {
+        Description: '',
+        DiscountedFrom: '100',
+        Interval: 'month',
+        Lookup: 'price_1HhIGXEoSnTpuGWGwN29a2zo',
+        Name: 'IoT - Pro ',
+        PlanGroup: 'pro',
+        Price: 70,
+        Priority: '30',
+        LicenseType: 'iot',
+        Featured: 'true',
+        SuccessRedirect: 'https://www.iot-ensemble.com/dashboard',
+        Devices: '50',
+        DataInterval: '30',
+        DataRetention: '604800',
+        HeaderName: 'Fathym | IoT Ensemble',
+        LicenseName: 'Fathym IoT',
+        PlanFeatures:
+          'IoT Management|Shared Cloud|50 Devices|30 second Data Velocity|7 Days of Data Retention',
+        TrialPeriodDays: 0,
+      },
+      {
+        Description: '',
+        DiscountedFrom: '1000',
+        Interval: 'year',
+        Lookup: 'price_1HhIGXEoSnTpuGWGMLhRZtbK',
+        Name: 'IoT - Pro ',
+        PlanGroup: 'pro',
+        Price: 700,
+        Priority: '30',
+        LicenseType: 'iot',
+        Featured: 'true',
+        SuccessRedirect: 'https://www.iot-ensemble.com/dashboard',
+        Devices: '50',
+        DataInterval: '30',
+        DataRetention: '604800',
+        HeaderName: 'Fathym | IoT Ensemble',
+        LicenseName: 'Fathym IoT',
+        PlanFeatures:
+          'IoT Management|Shared Cloud|50 Devices|30 second Data Velocity|7 Days of Data Retention',
+        TrialPeriodDays: 0,
+      },
+    ];
+   }
 
   public ngOnInit(): void { }
 
